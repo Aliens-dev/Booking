56d6ef810c21f7f890bd1aff4546b982b3bf8b31
aliensDev <merazganabil34@gmail.com>
2021-06-20 08:47:23 +0100

    client/renter rent

--- app/Http/Controllers/Users/UserRentController.php
+++ app/Http/Controllers/Users/UserRentController.php
@@ -6,6 +6,9 @@ namespace App\Http\Controllers\Users;
 
 use App\Http\Controllers\ApiController;
 use App\Models\Client;
+use App\Models\Property;
+use App\Models\Renter;
+use App\Models\Reservation;
 use App\Models\User;
 
 class UserRentController extends ApiController
@@ -13,35 +16,25 @@ class UserRentController extends ApiController
 
     public function __construct()
     {
-        $this->middleware(['auth:users', 'client.auth']);
+        $this->middleware(['auth:users']);
     }
 
     public function index($id)
     {
-        /*
-        if(auth()->id() != $id) {
-            return response()->json(['success' => false,'message' => 'unauthorized access'], 401);
-        }
-        */
-        $user = Client::find($id);
+        $user = User::find($id);
         if(is_null($user)) {
             return response()->json(['success' => false, 'message' => 'no record found'], 403);
         }
-
-        $properties = $user->properties()->withAll()->paginate(10);
-
-        foreach ($properties as $property) {
-            $property->reservation = [
-                'id' => (int)$property->pivot->id,
-                'renter_id' => (int)$property->pivot->renter_id,
-                'client_id' => (int)$property->pivot->client_id,
-                'property_id' => (int)$property->pivot->property_id,
-                'receipt_url' => $property->pivot->receipt_url,
-                'receipt_status' => $property->pivot->receipt_status,
-                'start_time' => $property->pivot->start_time,
-                'end_time' => $property->pivot->end_time,
-            ];
+        $reservations = [];
+        if($user->user_role == 'client') {
+            $reservations = Reservation::where('client_id', $id)->get();
+        }else if($user->user_role == 'renter') {
+            $reservations = Reservation::where('renter_id', $id)->where('receipt_status', 'approved')->get();
         }
-        return response()->json(['success' => true, 'message' => $properties],200);
+        foreach ($reservations as $reservation) {
+            $reservation->property = Property::find($reservation->property_id);
+        }
+        return response()->json(['success' => true, 'message' => $reservations],200);
+
     }
 }
